a31b851b98843ff236805c4adb2f6cc30549d44b
hirey-m <hirey.m@northeastern.edu>
2023-10-18 15:42:01 -0400

    opt moved

--- packer/app-setup.sh
+++ packer/app-setup.sh
@@ -6,5 +6,8 @@ sudo unzip webapp.zip -d web-app
 # Changing to webapp directory
 cd web-app
 
+# Moving users.csv to opt directory
+sudo mv /home/admin/web-app/users.csv /opt
+
 # Installing dependencies of nodejs
 sudo npm i
