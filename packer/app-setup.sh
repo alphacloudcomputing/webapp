3c049a0680d5497c6efdbb57ed0ed7aa58d731db
hirey-m <hirey.m@northeastern.edu>
2023-10-18 00:29:57 -0400

    new file

---
+++ packer/app-setup.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# moving into tmp directory
+cd /tmp
+
+# Unzipping application
+sudo unzip webapp.zip
+
+# Changing to webapp directory
+cd webapp
+
+# Installing dependencies of nodejs
+npm i
