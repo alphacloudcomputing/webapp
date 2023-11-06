62184ae843d50b77819a12bdf830f04a33a315e4
hirey-m <hirey.m@northeastern.edu>
2023-11-05 23:29:24 -0500

    cloudwatch

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ const logger = winston.createLogger({
   level: 'info',
   transports: [
       new winston.transports.Console({
-        format: winston.format.combine(winston.format.colorize(),winston.format.json(),customFormat),
+        format: winston.format.combine(winston.format.timestamp(),winston.format.json()),
       }),
       new winston.transports.File({
         filename: '/var/log/webapp.log',
